4585c71f9c455d08d34f689a61d50903658bbadd
Mark Baiden <markbaiden17@gmail.com>
2025-11-15 19:55:36 +0000

    Modify again daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 Task = input("Enter your task: ")
-Time_Bound = input("Is it time-bound (yes/no)? ")
 Priority = input("Priority (high/medium/low): ")
+Time_Bound = input("Is it time-bound (yes/no)? ")
 
 reminder_message = ""
 
