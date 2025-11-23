ea50f06872482890eb228eaf738cc1cf7a144d1a
Mark Baiden <markbaiden17@gmail.com>
2025-11-23 13:19:04 +0000

    Shopping List Manager - Task 1

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,50 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            #Prompt for and add item
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+            print(f"'{item}' has been added to the list.")
+
+        elif choice == '2':
+            #Prompt for and remove item
+            item = input("Enter the item to remove: ")
+
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"'{item}' has been removed from the list.")
+
+            else:
+                print(f"'{item}' not found. Please try again.")
+
+        elif choice == '3':
+            #Display shopping list
+            print("Your Current Shopping List: ")
+
+            if len(shopping_list) == 0:
+                print("Your shopping list is empty.")
+
+            else:
+                for item in shopping_list:
+                    print(f"- {item}")
+
+        elif choice == '4':
+            print('Goodbye!')
+
+            break
+
+        else:
+            print('Invalid choice. Please try again.')
+
+main()
